9eb0e8dd49ee17e526acfdbd7a5a3af536c52d1b
AmirAjam <amir.ajam2210@gmail.com>
2025-08-19 11:43:04 +0330

    Delete user via API

--- src/icons.js
+++ src/icons.js
@@ -3,7 +3,7 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import { LuChartLine, LuUser } from "react-icons/lu";
 import { PiMaskHappyLight } from "react-icons/pi";
 import { IoFastFoodOutline, IoClose } from "react-icons/io5";
-import { BiSearch } from "react-icons/bi";
+import { BiSearch,BiSort } from "react-icons/bi";
 import { IoIosMenu } from "react-icons/io";
 import { RiShoppingCartLine } from "react-icons/ri";
 import { IoIosNotificationsOutline,IoMdCart  } from "react-icons/io";
@@ -11,6 +11,8 @@ import { MdSpaceDashboard, MdCategory } from "react-icons/md"
 import { BsDatabaseFillAdd } from "react-icons/bs";
 import {HiOutlineUsers} from 'react-icons/hi2'
 import { FaUsers } from "react-icons/fa";
+import { RiSortAlphabetDesc,RiSortAlphabetAsc  } from "react-icons/ri";
+ 
 
 const icons = {
   UserCi: CiUser,
@@ -31,6 +33,9 @@ const icons = {
   AddProduct: BsDatabaseFillAdd,
   Category: MdCategory,
   Users: FaUsers,
+  SortAlphabetDesc:RiSortAlphabetDesc,
+  SortAlphabetAsc:RiSortAlphabetAsc,
+  Sort:BiSort,
 
 };
 
